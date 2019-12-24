6e724791c8ffd9010285427ada5284ff0d1ff55d
Ziurin Maksim <maksim.ziurin@rakuten.com>
2019-12-24 11:27:24 +0900

    Initial Commit

---
+++ NewsRedux/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsRedux
+//
+//  Created by Ziurin, Maksim on 2019/12/24.
+//  Copyright © 2019 Ziurin, Maksim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
